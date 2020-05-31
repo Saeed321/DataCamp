a03b22e249487bc4dfa2d24af9fcb43e9afacff8
Saeed Ullah <saeedonline12@gmail.com>
2020-06-01 00:32:07 +0200

    Analyzing Police Activity with pandas is added

---
+++ Data Scientist with Python/15. Analyzing Police Activity with pandas/4. Analyzing the effect of weather on policing/4.1. Comparing arrest rates by weather rating.py
@@ -0,0 +1,19 @@
+# Comparing arrest rates by weather rating
+
+# Do police officers arrest drivers more often when the weather is bad? Find out below!
+# •	First, you'll calculate the overall arrest rate.
+# •	Then, you'll calculate the arrest rate for each of the weather ratings you previously assigned.
+# •	Finally, you'll add violation type as a second factor in the analysis, to see if that accounts for any differences in the arrest rate.
+# Since you previously defined a logical order for the weather categories, good < bad < worse, they will be sorted that way in the results.
+
+# Calculate the overall arrest rate by taking the mean of the is_arrestedSeries.
+# Calculate the overall arrest rate
+print(ri_weather.is_arrested.mean())
+
+# Calculate the arrest rate for each weather rating using a .groupby()
+# Calculate the arrest rate for each 'rating'
+print(ri_weather.groupby('rating').is_arrested.mean())
+
+# Calculate the arrest rate for each combination of violation and rating. How do the arrest rates differ by group?
+# Calculate the arrest rate for each 'violation' and 'rating'
+print(ri_weather.groupby(['violation','rating']).is_arrested.mean())
