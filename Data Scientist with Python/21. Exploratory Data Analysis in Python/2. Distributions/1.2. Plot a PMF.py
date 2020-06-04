70c6f31c78caa04ed38717c04394e40f5350d858
Saeed Ullah <saeedonline12@gmail.com>
2020-06-04 02:17:08 +0200

    Exploratory Data Analysis in Python is added

---
+++ Data Scientist with Python/21. Exploratory Data Analysis in Python/2. Distributions/1.2. Plot a PMF.py
@@ -0,0 +1,32 @@
+# Plot a PMF
+
+# Now let's plot a PMF for the age of the respondents in the GSS dataset. The variable 'age' contains respondents' age in years.
+
+# •	Select the 'age' column from the gss DataFrame and store the result in age.
+
+# Select the age column
+age = gss['age']
+
+# •	Make a normalized PMF of age. Store the result in pmf_age.
+
+# Select the age column
+age = gss['age']
+
+# Make a PMF of age
+pmf_age = Pmf(age, normalize = True)
+
+# •	Plot pmf_age as a bar chart.
+
+# Select the age column
+age = gss['age']
+
+# Make a PMF of age
+pmf_age = Pmf(age)
+
+# Plot the PMF
+pmf_age.bar()
+
+# Label the axes
+plt.xlabel('Age')
+plt.ylabel('PMF')
+plt.show()
