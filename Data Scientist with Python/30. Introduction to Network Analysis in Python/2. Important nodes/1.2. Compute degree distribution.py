e48da506b0d6efb89831131a66ffe1917086e030
Saeed Ullah <saeedonline12@gmail.com>
2020-06-26 02:38:48 +0200

    First two chapters of Introduction to Network Analysis in Python is added

---
+++ Data Scientist with Python/30. Introduction to Network Analysis in Python/2. Important nodes/1.2. Compute degree distribution.py
@@ -0,0 +1,18 @@
+# Compute degree distribution
+
+# The number of neighbors that a node has is called its "degree", and it's possible to compute the degree distribution across the entire graph. In this exercise, your job is to compute the degree distribution across T.
+
+# Instructions
+
+# •	Use a list comprehension along with the .neighbors(n) method to get the degree of every node. The result should be a list of integers.
+# o	Use n as your iterator variable.
+# o	The output expression of your list comprehension should be the number of neighbors that node n has - that is, its degree. Use the len() and list() functions together with the .neighbors() method to compute this.
+# o	The iterable in your list comprehension is all the nodes in T, accessed using the .nodes() method.
+
+# •	Print the degrees.
+
+# Compute the degree of every node: degrees
+degrees = [len(list(T.neighbors(n))) for n in T.nodes()]
+
+# Print the degrees
+print(degrees)
